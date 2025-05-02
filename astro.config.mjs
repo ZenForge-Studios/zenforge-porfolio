9441f4c1f1eded1fc201e41dcc3d0f7b2ceb5c73
Angel Rivera <96215027+DevCuac@users.noreply.github.com>
2025-05-02 14:54:00 -0500

    Build #25

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://zenforge-studios.com",
   integrations: [tailwind()],
+  build: {
+    assets: "dist",
+    publicDir: "public" // ✅ Asegura que los archivos dentro de /public sean accesibles en producción
+  }
 });
