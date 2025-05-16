1903d61ba24501d895e0be6e9d6facf672481a9a
18Millions <higuera.joscar@gmail.com>
2025-05-15 21:38:36 -0400

    Se añade React y se mejora esteticamente en navegador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://zenforge-studios.com",
   integrations: [
     tailwind(),
+    react(),
     icon(), // 👈 Añadido correctamente aquí
   ],
   build: {
@@ -13,3 +15,4 @@ export default defineConfig({
     publicDir: "public", // ✅ Asegura que los archivos dentro de /public sean accesibles en producción
   }
 });
+
